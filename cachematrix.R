6ed2b5d73e31d3b1ea213a58d8956511d884bbcf
adilimani <adilimani@gmail.com>
2015-12-25 13:55:40 +0100

    wrote the function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The code below includes two functions, whose purpose is to cache the inverse of a Matrix
+## If Inverse Matrix exists in cache, then the program will utilize that rather than re-calculating
+## This should cut down on computationally intensive calculations when not required
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix function sets the value of matrix, gets the value of matrix,
+## sets a value for the inverse matrix for it, and gets the value of the inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    InvM <- matrix()
+    set <- function(y) {
+        x <<- y ##Defining x in the main (makeCacheMatrix) function
+        InvM <<- matrix() ##Defining InvM in the main (makeCacheMatrix) function
+    }
+    get <- function() x
+    setinverse <- function(inverse) InvM <<- inverse
+    getinverse <- function() InvM
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function checks if inverse has been calculated (and stored in cache)
+## Only if it does not have an inverse then it calculates it, and saves as matrix.
+## FInally, it saves down the new value of the inverse to th cache for future use. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    InvM <- x$setinverse()
+    if(!is.null(InvM)) { ##Checks if matrix exists and is not null
+        message("getting cached data")
+        return(InvM) #Returning from cache if it exists
+    }
+    data <- x$get()
+    InvM <- Solve(data, ...)%*%data ##Calculation of inverse, assumes an invertible matrix
+    x$setinverse(InvM) ##Saving to cache
+    InvM ## Return a matrix that is the inverse of 'x'
 }
